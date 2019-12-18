b8cc3329f1496e0e2de93b39c0958b9b10a8db1a
Akiraff <akiraff@hotmail.com>
2019-12-18 12:05:34 +0800

    minor changes

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -7,13 +7,6 @@
 		    	<p>An inventory management system, built-in with cutting edge technology. Our goal is to ensure, wherever and whenever you are, the BeEz will provide up-to-date inventories of your storage at real time. Easy to set-up, with an exceptional low learning curve which is greate for people with non-traditional technical background.</p>
     		</div>
     	</div>
-
-    	<div class="row">
-    		<div class="col-md-4">
-    			<h3>Pricing</h3>
-    			<p>Starting at 750HKD/mo*</p>
-    		</div>
-    	</div>
     </div>
   </div>
 </template>
